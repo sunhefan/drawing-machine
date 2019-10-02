426e9b6bc3b4bc7999b6ad55de503152079aa688
HeFan Lim <hefanlim@HeFans-MBP.hsd1.ca.comcast.net>
2019-10-01 21:34:58 -0700

    added drawing machines

--- sketch.js
+++ sketch.js
@@ -1,7 +1,23 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+  background(72, 201, 176);
+  strokeWeight(5);
+  
 }
 
 function draw() {
-  background(220);
+  stroke(map(mouseX, 0, 500, 50, 255, true))
+  if (mouseIsPressed) {
+    line(width - mouseX, height - mouseY, width - pmouseX, width - pmouseY);
+    line(mouseX, mouseY, pmouseX, pmouseY);
+  }
+}
+
+function keyTyped(){
+  //console.log('key ${key} is being typed')
+//  console.log("key" + key +"is being typed")
+  if (key === 's') {
+ saveCanvas('fileName', 'png');
+  }
+  return false;
 }
