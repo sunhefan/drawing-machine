ab9800de6f2e4ac6abd31b3fed55f1b94643673f
HeFan Lim <hefanlim@HeFans-MacBook-Pro.local>
2019-10-03 17:49:20 -0700

    added noise

--- sketch.js
+++ sketch.js
@@ -1,23 +1,37 @@
+let noiseOffset = 0.0;
+let strokeWidth = 30;
+
+
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(windowWidth, windowHeight);
   background(72, 201, 176);
-  strokeWeight(5);
-  
+
+
+
 }
 
 function draw() {
+  background(72, 201, 176, 1);
+  strokeWeight(strokeWidth);
+  noiseOffset += 3;
+  strokeWidth = noise(noiseOffset*100) * 100;
   stroke(map(mouseX, 0, 500, 50, 255, true))
+
+
   if (mouseIsPressed) {
-    line(width - mouseX, height - mouseY, width - pmouseX, width - pmouseY);
-    line(mouseX, mouseY, pmouseX, pmouseY);
+
+    point(width - mouseX, height - mouseY, width - pmouseX, width - pmouseY);
+
+    // line(width - mouseX, height - mouseY, width - pmouseX, width - pmouseY);
+    // line(mouseX, mouseY, pmouseX, pmouseY);
   }
 }
 
-function keyTyped(){
+function keyTyped() {
   //console.log('key ${key} is being typed')
-//  console.log("key" + key +"is being typed")
+  //  console.log("key" + key +"is being typed")
   if (key === 's') {
- saveCanvas('fileName', 'png');
+    saveCanvas('fileName', 'png');
   }
   return false;
 }
