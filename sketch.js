6308569ada886a47285eab86a5f60ec678e4d718
HeFan Lim <hefanlim@HeFans-MBP.hsd1.ca.comcast.net>
2019-12-08 16:19:30 -0800

    changed
    changed

--- sketch.js
+++ sketch.js
@@ -7,8 +7,7 @@ let strokeWidth = 3;
 // let a= 50;
 // let b =100;
 let xoff = 10;
-
-
+let num = 100;
 
 
 
@@ -33,19 +32,20 @@ function draw() {
     // background (col,a,b);
 
 
-
-
     noiseOffset += 0.05;
     strokeWidth = noise(noiseOffset * 5) * 200;
     fill(frameCount % 360, 100, 100);
 
     // stroke(map(mouseX, 0, 500, 50, 255, true))
 
-noStroke();
+    noStroke();
 
-    rect(width- mouseX, height- mouseY,  width/2-pmouseX, height/2 -pmouseY)
-    for (let i=1; i<5; i++){
-  rect(mouseX, mouseY,100, 100);}
+    rect(width - mouseX, height - mouseY, width / 2 - pmouseX, height / 2 - pmouseY)
+
+    for (let i = 1; i < 5; i++) {
+      fill(frameCount % 360, 30, 30)
+      rect(mouseX, mouseY, 80, 80);
+    }
 
   }
   return false;
